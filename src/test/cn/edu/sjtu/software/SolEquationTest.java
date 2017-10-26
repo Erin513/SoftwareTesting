3a43f32983e5a5a97d1ed5fcb6bc9c18a882416d
dagenian <dagenian@163.com>
2017-10-26 21:36:21 +0800

    add SolEquationTest

---
+++ src/test/cn/edu/sjtu/software/SolEquationTest.java
@@ -0,0 +1,24 @@
+package cn.edu.sjtu.software;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolEquationTest {
+    @Test
+    public void isSolvable() throws Exception {
+    }
+
+    @Test
+    public void isSolvable1() throws Exception {
+    }
+
+    @Test
+    public void rootRes() throws Exception {
+    }
+
+    @Test
+    public void rootRes1() throws Exception {
+    }
+
+}
