5166e4871ac25232bb287b5b1f4a2a43f4cbccdf
Yun Wang <199410152@qq.com>
2017-10-26 15:27:17 +0800

    ver0.1

---
+++ src/main/cn/edu/sjtu/software/solEquation.java
@@ -0,0 +1,68 @@
+package cn.edu.sjtu.software;
+
+public class solEquation {
+
+    private int a = 1;
+    private int b = 1;
+    private int c = 1;
+
+    double delta;
+    double[] rootRes = new double[2];
+
+    public Equation(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public boolean isSolvable() {
+        delta = b * b - 4 * a * c;
+        return delta < 0;
+    }
+
+    public static boolean isSolvable(int a, int b, int c) {
+        solEquation equ = new solEquation(a,b,c);
+        return equ.isSolvable;
+    }
+
+    public double[] rootRes() {
+        if (isSolvable) {
+            rootRes[0] = ( - b + Math.sqrt(delta))/(2*a);
+            rootRes[1] = (-b - Math.sqrt(delta))/(2*a);
+        }
+        else {
+            rootRes[0] = null;
+            rootRes[1] = null;
+        }
+            return rootRes;
+    }
+
+    public static double[] rootRes(int a, int b, int c) {
+        solEquation equ = new solEquation(a,b,c);
+        return equ.rootRes;
+    }
+}
