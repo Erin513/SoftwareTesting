95ded9753564cd602ee51615e58b03a6638dab4c
Erin <erin@ErindeMacBook-Pro.local>
2017-10-27 10:52:12 +0800

    update CalRadius

--- src/main/cn/edu/sjtu/software/CalRadius.java
+++ src/main/cn/edu/sjtu/software/CalRadius.java
@@ -59,7 +59,7 @@ public class CalRadius {
             System.out.println("三点共线");
             return null;
         } else {
-            Point center = p1;
+            Point center = new Point(0, 0);
             double x1 = p1.x, y1 = p1.y;
             double x2 = p2.x, y2 = p2.y;
             double x3 = p3.x, y3 = p3.y;
